f74a3880b145b8a6612965467daf79cdb78e6889
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-07-26 10:57:29 +0200

    add sorting

--- src/Movies.js
+++ src/Movies.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { getData } from "./data";
 import './Movies.css';
 
@@ -6,6 +6,7 @@ export const Movies = () => {
     const [movies, setMovies] = useState([]);
     const [error, setError] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
+    const [sortingType, setSortingType] = useState(null);
 
     const displayStars = (number) => {
         const stars = [];
@@ -32,6 +33,39 @@ export const Movies = () => {
         })
     }
 
+    const handleSortTypeChange = (value) => {
+        setSortingType(value);
+    }
+
+    const sortMovies = (sortingType, movies) => {
+        switch (sortingType) {
+            case 'title':
+                return [...movies].sort(
+                    (movieA, movieB) => movieA.title.localeCompare(movieB.title),
+                );
+            case 'year':
+                return [...movies].sort(
+                    (movieA, movieB) => movieB.year - movieA.year,
+                );
+            case 'director':
+                return [...movies].sort(
+                    (movieA, movieB) => movieA.director.localeCompare(movieB.director),
+                );
+            case 'rating':
+                return [...movies].sort(
+                    (movieA, movieB) => movieB.rating - movieA.rating,
+                );
+            default:
+                return movies;
+        }
+    };
+
+    const currentMovies = useMemo(() => {
+        let sortedMovies = sortMovies(sortingType, movies);
+    
+        return sortedMovies;
+      }, [movies, sortingType]);
+
     useEffect(() => {
         handleReload();
     }, []);
@@ -50,6 +84,23 @@ export const Movies = () => {
         <div>
 
         {!error && (
+            <>
+            <div className="sorter">
+                <label className="sorter_label--select">Sort movies by</label>
+                <select
+                    id="select"
+                    className="sorter__items-selector"
+                    defaultValue="default"
+                    onChange={(event) => handleSortTypeChange(event.target.value)}
+                >
+                    <option value='default'>Id</option>
+                    <option value='title'>Title</option>
+                    <option value='year'>Year</option>
+                    <option value='director'>Director</option>
+                    <option value='rating'>Rating</option>
+                </select>
+            </div>
+
             <table className="movie__table">
                 <tr className="table__header">
                     <th className="movie__id">Id</th>
@@ -59,7 +110,7 @@ export const Movies = () => {
                     <th className="movie__rating">Rating</th>
                 </tr>
                 <tbody>
-                    {movies.map(movie => (
+                    {currentMovies.map(movie => (
                     <tr key={movie.id} className="movie">
                         <td className="movie__id">{movie.id}</td>
                         <td className="movie__title">{movie.title}</td>
@@ -70,6 +121,7 @@ export const Movies = () => {
                     ))}
                 </tbody>
             </table>
+            </>
         )}
         </div>
     )
